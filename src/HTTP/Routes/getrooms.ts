0ee3a9b1a59fb4c1484010c8257c959f4aea4917
Vitor <vitor3araujo@gmail.com>
2025-07-09 19:01:48 -0300

    Changes in client and getrooms files

--- src/HTTP/Routes/getrooms.ts
+++ src/HTTP/Routes/getrooms.ts
@@ -1,10 +1,14 @@
 import type { FastifyPluginCallbackZod } from 'fastify-type-provider-zod'
 import { db } from '../../db/connection.ts'
 import { schema } from '../../db/schema/index.ts'
+import { id } from 'zod/v4/locales'
 
 export const getRoomsRoute: FastifyPluginCallbackZod = (app) => {
   app.get('/rooms',  async () => {
-    const results = await db.select().from(schema.rooms).orderBy(schema.rooms.createdAt)
+    const results = await db.select({
+      id: schema.rooms.id,
+      name: schema.rooms.name,
+    }).from(schema.rooms).orderBy(schema.rooms.createdAt)
 
     return results
   })
